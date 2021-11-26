7cdfab831f1623e19d2cb7a73eaaa128e4b22a69
Tetsuya Morimoto <tetsuya.morimoto@gmail.com>
2021-11-26 13:01:49 +0900

    カスタム github actions の初期実装を追加する

--- README.md
+++ README.md
@@ -1,1 +1,130 @@
 # hr-library-auto-update
+
+[maven](https://maven.apache.org/) でパッケージ管理を行うプロジェクトの依存ライブラリのバージョンを自動更新するためのカスタム GitHub Action です。プロジェクト非依存で利用します。
+
+## 前提条件
+
+* [Maven Wrapper](https://github.com/takari/maven-wrapper) (mvnw スクリプト) をリポジトリのルートディレクトリに置く
+* [Versions Maven Plugin](https://www.mojohaus.org/versions-maven-plugin/) でバージョン管理を行う
+  * 次のような設定を `pom.xml` に追加する
+
+```xml
+    <build>
+        <plugins>
+            <plugin>
+                <groupId>org.codehaus.mojo</groupId>
+                <artifactId>versions-maven-plugin</artifactId>
+                <version>2.8.1</version>
+                <configuration>
+                </configuration>
+            </plugin>
+        </plugins>
+    </build>
+```
+
+## 使い方
+
+```yml
+- name: Update library versions
+  uses: HoshinoResort/hr-library-auto-update@init-dev
+  with:
+    # maven コマンドで実行するテスト向けのコマンドを指定する
+    # デフォルト: test
+    maven-test-command: ''
+
+    # maven コマンドの --settings オプションに渡す settings.xml への相対パス
+    # デフォルト: 未使用
+    maven-settings-xml-path: ''
+
+    # Versions Maven Plugin のアップデートルールを記述する rules.xml への相対パス
+    # デフォルトでは本リポジトリにある rules.xml の設定を利用する
+    # デフォルト: 未使用
+    custom-rules-xml-path: ''
+
+    # versions:display-dependency-updates でバージョンの更新をチェックする
+    # このコマンドはすべての依存ライブラリに対してメジャーバージョンの更新も含めてチェックする
+    # 不要なら無効にすることで処理時間を短縮できる
+    # デフォルト: 'false'
+    display-dependency-updates: ''
+
+    # GitHub Actions が提供するビルドキャッシュを利用するかどうか
+    # ビルドキャッシュを利用することでバージョンチェックの処理を高速化できる
+    # デフォルト: 'true'
+    use-cache: ''
+
+    # テストコマンドが成功したときにバージョンの更新をリポジトリに反映するかどうか
+    # バージョンのアップデートがあるかどうかをチェックしたいだけなら無効でよい
+    # デフォルト: 'false'
+    push-on-success: ''
+```
+
+### 呼び出し側の設定例
+
+```yml
+- name: Update library versions
+  uses: HoshinoResort/hr-library-auto-update@v1
+  with:
+    maven-test-command: "test -DfailIfNoTests=false -Dtest='!IntegrationTest'"
+    maven-settings-xml-path: "${{ github.workspace }}/settings.xml"
+    push-on-success: 'true'
+```
+
+リポジトリに push するには GITHUB_TOKEN や permissions といった認証情報を適切に設定する必要があります。
+
+```yml
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    permissions:
+      contents: write
+    env:
+        GITHUB_TOKEN: ${{ secrets.MY_TOKEN }}
+```
+
+#### リファレンス
+
+* [GitHub Actions / Security guides / Automatic token authentication](https://docs.github.com/ja/actions/security-guides/automatic-token-authentication)
+* [GitHub Actions / Learn GitHub Actions / Workflow syntax / permissions](https://docs.github.com/ja/actions/learn-github-actions/workflow-syntax-for-github-actions#onworkflow_dispatchinputs)
+
+## FAQ
+
+* 最新のメジャーバージョンにアップデートしません
+  * 事故を防ぐ意図で `allowMajorUpdates=false` で防止しています
+  * メジャーバージョンを上げたいときは手動でプロジェクトの `pom.xml` のバージョンを更新してください
+* 特定の文字列を含むバージョンを無視してほしい
+  * 汎用的なバージョン文字列であれば本リポジトリの `rules.xml` に含めるのでイシューを作成してください
+  * プロジェクト特有の要件であれば、自プロジェクトで `rules.xml` を定義して `custom-rules-xml-path` で指定してください
+* こういった機能や設定を追加してほしい
+   * プロジェクト非依存の汎用的な機能であれば検討するのでイシューを作成してください
+
+## 開発者向け
+
+基本的にローカルで maven コマンドを実行して検証できるよう `functions.sh` にシェル関数として実装しています。機能拡張するときはローカルでデバッグができることを意識して開発してください。
+
+### デバッグ
+
+`functions.sh` を読み込んで直接シェル関数を呼び出します。
+
+```bash
+$ source path/to/hr-library-auto-update/functions.sh && mvnw_version
+
+$ ./mvnw --no-transfer-progress --version
+Apache Maven 3.6.3 (cecedd343002696d0abb50b32b541b8a6ba2883f)
+Maven home: path/to/.m2/wrapper/dists/apache-maven-3.6.3-bin/1iopthnavndlasol9gbrbg6bf2/apache-maven-3.6.3
+Java version: 11.0.11, vendor: Ubuntu, runtime: /usr/lib/jvm/java-11-openjdk-amd64
+Default locale: ja_JP, platform encoding: UTF-8
+OS name: "linux", version: "5.11.0-40-generic", arch: "amd64", family: "unix"
+```
+
+### テスト
+
+まとめてテストを実行するために `do_test` というシェル関数を定義しています。
+
+```bash
+$ source functions.sh && do_test
+```
+
+## リファレンス
+
+* [Versions Maven Plugin](https://www.mojohaus.org/versions-maven-plugin/index.html)
+* [GitHub Actions / アクションの作成](https://docs.github.com/ja/actions/creating-actions)
---
+++ functions.sh
@@ -0,0 +1,187 @@
+#!/bin/bash
+
+## Color
+RED='\033[0;31m'
+BG_RED='\033[0;41m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+## Clear the color after that
+CLEAR='\033[0m'
+
+## Git
+USER_EMAIL="hr-library-auto-update@users.noreply.github.com"
+USER_NAME="github-action"
+
+## CLI
+MVNW="./mvnw"
+SETTINGS_XML=
+RULES_XML=
+
+declare -a DEFAULT_USE_LATEST_RELEASES_PARAMETERS=(
+    "generateBackupPoms=false"
+    "allowMajorUpdates=false"
+)
+
+## functions
+function is_executable() {
+    local path="$1"
+    test -x "$path"
+    if [[ $? -ne 0 ]]; then
+        error "${path} is not found or executable"
+        return 1
+    fi
+}
+
+function run() {
+    local path="$(which $1)"
+    local arguments="${@:2}"
+    is_executable "$path"
+    if [[ $? -eq 0 ]]; then
+        echo "$ ${path} ${arguments}"
+        eval "${path} ${arguments}"
+    fi
+}
+
+function print_color() {
+    local color="$1"
+    local message="$2"
+    echo -e "${color}${message}${CLEAR}"
+}
+
+function success() {
+    local message="SUCCESS: $1"
+    print_color $GREEN "$message"
+}
+
+function info() {
+    local message="INFO: $1"
+    print_color $BLUE "$message"
+}
+
+function error() {
+    local message="ERROR: $1"
+    print_color $BG_RED "$message"
+}
+
+function get_params() {
+    local name="$1"
+    local params=""
+    for param in $(eval echo '${'$name'[@]}')
+    do
+        params="${params} -D${param}"
+    done
+    echo $params
+}
+
+function set_settings_xml() {
+    local path="$1"
+    test -f "$path"
+    SETTINGS_XML="$path"
+    info "use maven settings: $SETTINGS_XML"
+}
+
+function set_rules_xml() {
+    local path="$1"
+    test -f "$path"
+    RULES_XML="$path"
+    info "use comparison rules: $RULES_XML"
+}
+
+function run_mvnw() {
+    local arguments="$@"
+    local params="--no-transfer-progress"
+    if [[ -n "$SETTINGS_XML" ]]; then
+        params="${params} --settings \"$SETTINGS_XML\""
+    fi
+    run $MVNW $params $arguments
+}
+
+function mvnw_version() {
+    run_mvnw --version
+}
+
+function mvnw_latest_releases() {
+    local params=$(get_params "DEFAULT_USE_LATEST_RELEASES_PARAMETERS")
+    params="${params} -Dmaven.version.rules=file://${RULES_XML}"
+    run_mvnw "versions:use-latest-releases" ${params}
+}
+
+function get_modified_poms() {
+    local files=""
+    for f in $(git diff --name-only -- '*pom.xml')
+    do
+        files="$files $f"
+    done
+    echo $files
+}
+
+function has_modified_poms() {
+    if [[ -n "$(get_modified_poms)" ]]; then
+        echo "true"
+    else
+        echo "false"
+    fi
+}
+
+function configure_system_account() {
+    run git config --global user.email "$USER_EMAIL"
+    run git config --global user.name "$USER_NAME"
+}
+
+function commit_pom_xml() {
+    local files=$(get_modified_poms)
+    if [[ -n "$files" ]]; then
+        run git diff $files
+        run git add $files
+        run git commit -m "\"update dependencies\""
+    else
+        info "no update for dependencies"
+    fi
+}
+
+function push_to_remote() {
+    run git push origin
+}
+
+################################
+## from here for testing
+################################
+function print_header() {
+    local message="$1"
+    echo "################################################################"
+    echo "# ${message}"
+    echo "################################################################"
+}
+
+function do_test() {
+    local path="$1"
+
+    print_header "call mvnw_version"
+    mvnw_version
+    echo
+
+    print_header "call set_rules_xml"
+    set_rules_xml "$(pwd)/rules.xml"
+    echo
+
+    print_header "call run_mvnw for displaying updates"
+    run_mvnw "versions:display-dependency-updates"
+    echo
+
+    print_header "call mvnw_latest_releases"
+    mvnw_latest_releases
+    echo
+
+    print_header "call has_modified_poms"
+    echo $(has_modified_poms)
+    echo
+
+    print_header "call commit_pom_xml"
+    commit_pom_xml
+    echo
+
+    success "test was completed"
+}
